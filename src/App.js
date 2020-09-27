3b42569feb188ba25e7c6726957f2b7dec4bb527
Evan Aspaas <aspaasevan@gmail.com>
2020-09-27 13:08:41 -0700

    applies createStrucutreSelectord to all current files using mapStateToProps

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -12,6 +13,7 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 function App(props) {
   // firebase related
@@ -54,8 +56,8 @@ function App(props) {
 };
 
 // brings in state as props from redux store 
-const mapStateToProps = ({ user: { currentUser } }) => ({ 
-  currentUser,
+const mapStateToProps = createStructuredSelector({ 
+  currentUser: selectCurrentUser,
 });
 
 // sends out a dispatch to set the current user 
