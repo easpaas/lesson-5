ff538294a0d43b1b26fd7ae07c1a2d153f817118
Evan Aspaas <aspaasevan@gmail.com>
2020-12-04 14:01:30 -0800

    test API key from stripe added, crown favicon added and title updated

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -4,7 +4,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_live_51GylP3A767XgCBwdtUsQ4FW77fsj1qQ7gJ2L5quLRA7RELibkJvyok5adOPK6cIGKeQUbsq2khDsQPB0so0oH05800MmcKWLKI';
+  const publishableKey = 'pk_test_51GylP3A767XgCBwd2XvHeShLOM8dN8x05sHjnHF4rjEZE9292GDZsdFy4VVGVgTIJAEeIhQQ8BFrSXft7COhWMHT00ignIcL00';
 
   const onToken = token => {
     console.log(token)
