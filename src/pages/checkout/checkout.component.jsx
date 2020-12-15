85b40d0d1f8d5cfd206549795528105564393c04
Evan Aspaas <aspaasevan@gmail.com>
2020-12-15 14:43:30 -0800

    adds margin, font-size and text-align to test class

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -36,6 +36,7 @@ const CheckoutPage = ({ cartItems, total }) => {
     }
     <div className="test">
       *Please use the following test credit card for payments*
+      <br/>
       4242 4242 4242 4242 - Exp: 12/22 - CVV: 666
     </div>
     <div className='total'>
