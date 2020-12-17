84de11d45ca15746f7662dc5142c85c115f05260
Evan Aspaas <aspaasevan@gmail.com>
2020-12-17 13:43:02 -0800

    firebase auth signin working

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,13 +4,16 @@ import 'firebase/auth';
 
 
 const config = {
+    // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
     apiKey: "AIzaSyDd_puMLmij0RPoBbJNjZJlQ7GPVXa1BUk",
     authDomain: "e-commerce-project-8cdbc.firebaseapp.com",
     databaseURL: "https://e-commerce-project-8cdbc.firebaseio.com",
     projectId: "e-commerce-project-8cdbc",
     storageBucket: "e-commerce-project-8cdbc.appspot.com",
     messagingSenderId: "562325095295",
-    appId: "1:562325095295:web:eac7ca1ffe5daa329f904a"
+    appId: "1:562325095295:web:eac7ca1ffe5daa329f904a",
+    measurementId: "G-PXB9098L77"
 };
 
 firebase.initializeApp(config);
